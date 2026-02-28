f7b52e88404f25b22276adb5312318ef69a90884
Sahana B S <sahana132006@gmail.com>
2026-02-28 15:47:04 +0530

    Time: 0 ms (100%), Space: 29.4 MB (99.63%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,19 +3,23 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k=k%n;
-        vector<int> temp(k);
-        int j=0;
-        for(int i=0;i<k;i++){
-            temp[i]=nums[n+i-k];
-        }
+        // vector<int> temp(k);
+        // int j=0;
+        // for(int i=0;i<k;i++){
+        //     temp[i]=nums[n+i-k];
+        // }
        
-        for(int i=n-1;i>=k;i--){
-            nums[i]=nums[i-k];
-        }
+        // for(int i=n-1;i>=k;i--){
+        //     nums[i]=nums[i-k];
+        // }
 
-         for(int i=0;i<k;i++){
-            nums[i]=temp[i];
-        }
+        //  for(int i=0;i<k;i++){
+        //     nums[i]=temp[i];
+        // This is brute approach}
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+      
 
 
     }      
