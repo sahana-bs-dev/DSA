c1c336abf74ea8a4c2d630d9d70eed7ad89bd2b0
Sahana B S <sahana132006@gmail.com>
2026-03-03 21:35:35 +0530

    Time: 0 ms (100%), Space: 20.5 MB (82.78%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-       for(int i=0;i<nums.size();i++){
-        int num = nums[i];
-        int count = 0;
-        for(int j=0;j<nums.size();j++){
-            if(num==nums[j]){
-                count++;
-            }
-        }
-        if(count==1){
-            return num;
-        }
+    //    sort(nums.begin(),nums.end());
+    //    for(int i=0;i<nums.size();i++){
+    //     int num = nums[i];
+    //     int count = 0;
+    //     for(int j=0;j<nums.size();j++){
+    //         if(num==nums[j]){
+    //             count++;
+    //         }
+    //     }
+    //     if(count==1){
+    //         return num;
+    //     }
        
-       } 
-       return nums[nums.size()-1];
+    //    } 
+    //    return nums[nums.size()-1];  This is the brute force method
+    int res=0;
+    for(int i=0;i<nums.size();i++){
+        res=res^nums[i];
+    }
+    return res;
     }
     
 };
