1965e2d031efc98f57cbd067e57f6a247c707354
Sahana B S <sahana132006@gmail.com>
2026-03-10 22:26:48 +0530

    Time: 1954 ms (5.01%), Space: 74 MB (95.84%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size() && j-i<=k;j++){
+                if(nums[i]==nums[j]){
+                    if(abs(i-j)<=k){
+                        return true;
+                    }
+                }
+            }
+        }
+       return false;
+    }
+};
