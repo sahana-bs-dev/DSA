cee30b7fcd6d2b5eb87b15bee6f356a2bfb3a404
Sahana B S <sahana132006@gmail.com>
2026-03-22 22:47:16 +0530

    Time: 0 ms (100%), Space: 9 MB (44.82%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        int product;
-        int max1=INT_MIN;
-        int max2=INT_MIN;
-        while(n>0){
-            int rem = n%10;
-            if(rem>max1){
-                max2=max1;
-                max1=rem;
-            }
-            else if(rem>max2){
-                max2=rem;
-            }
-            n=n/10;
-        }
-        product=max1*max2;
-       return product; 
+    //     int product;
+    //     int max1=INT_MIN;
+    //     int max2=INT_MIN;
+    //     while(n>0){
+    //         int rem = n%10;
+    //         if(rem>max1){
+    //             max2=max1;
+    //             max1=rem;
+    //         }
+    //         else if(rem>max2){
+    //             max2=rem;
+    //         }
+    //         n=n/10;
+    //     }
+    //     product=max1*max2;
+    //    return product; 
+    vector<int> digits;
+    while(n>0){
+        int rem = n%10;
+        digits.push_back(rem);
+        n=n/10;
+    }
+    sort(digits.begin(),digits.end());
+    return digits[digits.size()-2]*digits[digits.size()-1];
     }
 };
