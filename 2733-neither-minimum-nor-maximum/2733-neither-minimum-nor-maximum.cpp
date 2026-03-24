c91a6ee06687a3c04cd09dc9c0f87cf5a1e4461a
Sahana B S <sahana132006@gmail.com>
2026-03-24 21:46:29 +0530

    Time: 8 ms (25.12%), Space: 97 MB (99.28%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
-        int max_val=*max_element(nums.begin(),nums.end());
-        int min_val=*min_element(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=max_val && nums[i] != min_val){
-                return nums[i];
-            }
-        }
-       return -1; 
+    //     int max_val=*max_element(nums.begin(),nums.end());
+    //     int min_val=*min_element(nums.begin(),nums.end());
+    //     for(int i=0;i<nums.size();i++){
+    //         if(nums[i]!=max_val && nums[i] != min_val){
+    //             return nums[i];
+    //         }
+    //     }
+    //    return -1; 
+    if(nums.size()<3){
+        return -1;
+    }
+    sort(nums.begin(),nums.end());
+    return nums[1];
     }
 };
